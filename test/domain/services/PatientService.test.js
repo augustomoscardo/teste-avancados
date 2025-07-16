2dc56327ba94bff9248dd3c77de3d42d9e2f3f8c
Augusto Moscardo <augustomsaluz@gmail.com>
2025-07-15 23:22:09 -0300

    testando services

---
+++ test/domain/services/PatientService.test.js
@@ -0,0 +1,8 @@
+import { expect } from "chai";
+import sinon from "sinon";
+import { PatientService } from "../../../src/domain/services/PatientService.js";
+import { Patient } from "../../../src/domain/entities/Patient.js";
+import { PatientRepository } from "../../../src/infrastructure/persistence/PatientRepository.js";
+
+describe("PatientService", () => {
+});
